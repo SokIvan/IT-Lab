c81aa52b85775bd238676b58b370a3a437422056
SokIvan <ivansokolov2003@gmail.com>
2022-11-17 19:09:12 +0300

    exp_and_log

--- new_double_3/Double_union/Double_union/Double_union.cpp
+++ new_double_3/Double_union/Double_union/Double_union.cpp
@@ -1,133 +1,4 @@
-﻿#include <iostream>
-#include <climits>
-#include <float.h>
-#include <exception>
-#include <cmath>
-
-using std::cout;
-using std::endl;
-
-using std::isinf;
-
-using std::invalid_argument;
-
-
-
-union Dbl
-{
-    double f;
-    struct
-    {
-        uint64_t man : 52;
-        uint64_t ex : 11;
-        uint64_t sign : 1;
-    };
-};
-
-
-template<int e, int m>
-class DOUBLE
-{
-private:
-
-public:
-
-    Dbl D;
-
-    unsigned long long int get_mask()
-    {
-        unsigned long long int one = 1;
-        unsigned long long int mask = (((one << (m + 11)) - 1) << 1) + 1;
-        mask = mask << (52 - m);
-
-        return mask;
-    }
-
-    DOUBLE()
-    {
-        D.f = 0.0;
-    }
-    DOUBLE(const double& a)
-    {
-        D.f = a;
-        CHECK_EXP_AND_MAN();
-    }
-    DOUBLE(const DOUBLE& a)
-    {
-        D.f = a.D.f;
-    }
-
-    DOUBLE& operator=(const DOUBLE& a)
-    {
-        D.f = a.D.f;
-        return *this;
-    }
-    DOUBLE operator+(const DOUBLE& a)
-    {
-        DOUBLE res(*this);
-        res.D.f += a.D.f;
-        res.CHECK_EXP_AND_MAN();
-        return res;
-    }
-    DOUBLE operator-(const DOUBLE& a)
-    {
-        DOUBLE res(*this);
-        if (isinf(res.D.f) && isinf(a.D.f) && res.D.sign == a.D.sign) throw  invalid_argument("Attempted to substract Infinity and Infinity\n");
-        res.D.f -= a.D.f;
-        res.CHECK_EXP_AND_MAN();
-        return res;
-    }
-    DOUBLE operator*(const DOUBLE& a)
-    {
-        DOUBLE res(*this);
-        if (isinf(res.D.f) && a.D.f == 0.0 || isinf(a.D.f) && res.D.f == 0.0) throw  invalid_argument("Attempted to multiplicate Zero and Infinity\n");
-        res.D.f *= a.D.f;
-        res.CHECK_EXP_AND_MAN();
-        return res;
-    }
-    DOUBLE operator/(const DOUBLE& a)
-    {
-        DOUBLE res(*this);
-        if (a.D.f == 0.0) throw  invalid_argument("Attempted to divide by Zero\n");
-        res.D.f /= a.D.f;
-        res.CHECK_EXP_AND_MAN();
-        return res;
-    }
-
-    bool Check_expMAX()
-    {
-        return ((1024 + ((1 << (e - 1)) - 1)) >= D.ex);
-    }
-    bool Check_expMIN()
-    {
-        return ((1023 - ((1 << (e - 1)) - 1)) <= D.ex);
-    }
-
-    void special_infinity_and_null_check()
-    {
-        D.man &= (get_mask() * Check_expMAX());
-        D.ex |= ((1 << 11) - 1) * (unsigned long long int)!(Check_expMAX());
-
-        D.man &= (get_mask() * Check_expMIN());
-        D.ex &= ((1 << 11) - 1) * (unsigned long long int)(Check_expMIN());
-    }
-
-    void CHECK_EXP_AND_MAN()
-    {
-        special_infinity_and_null_check();
-
-    }
-
-
-    void get_double()
-    {
-        cout << "Double: " << D.f << endl;
-        cout << "sign: " << D.sign << endl;
-        cout << "exp: " << D.ex << endl;
-        cout << "mantissa: " << D.man << endl;
-    }
-
-};
+﻿#include "Floating_Point.h"
 
 
 
@@ -136,4 +7,73 @@ public:
 int main()
 {
 
+	float var1 = 32.423;
+	float var2 = 4.23;
+
+	Floating_Point<11, 52> a(var1);
+	Floating_Point<11, 52> q(var2);
+	a = a.Log(a, q);
+
+	cout << " Log(x1,x2) :" << endl << endl;
+	cout << "My log: " << a.D.f << endl << endl;
+	cout << "Real log: " << logf(var1) / logf(var2) << endl << endl << endl;
+
+
+
+	Floating_Point<11, 52> v1(123.231);
+
+	float f1 = logf(123.231);
+	Floating_Point<11, 52> ch1(f1);
+
+	v1 = v1.Log2(v1);
+
+	cout << " Log2 :" << endl << endl;
+	cout << "My log: " << v1.D.f << endl << endl;
+	cout << "Real log: " << log2f(123.231) << endl << endl << endl;
+	if ((v1.D.man == ch1.D.man) && (v1.D.ex == ch1.D.ex) && (v1.D.ex == ch1.D.ex)) cout << "They equal" << endl;
+	else cout << "They not equal" << endl;
+
+
+	Floating_Point<11, 52> v2(123.231);
+
+	float f2 = logf(123.231);
+	Floating_Point<11, 52> ch2(f2);
+
+	v2 = v2.Log10(v2);
+
+	cout << " Log10 :" << endl << endl;
+	cout << "My log: " << v2.D.f << endl << endl;
+	cout << "Real log: " << log10f(123.231) << endl << endl << endl;
+	if ((v2.D.man == ch2.D.man) && (v2.D.ex == ch2.D.ex) && (v2.D.ex == ch2.D.ex)) cout << "They equal" << endl;
+	else cout << "They not equal" << endl;
+
+
+	Floating_Point<11, 52> v3(123.231);
+
+	float f3 = logf(123.231);
+	Floating_Point<11, 52> ch3(f3);
+
+	v3 = v3.LogE(v3);
+
+	cout << " LogE :" << endl << endl;
+	cout << "My log: " << v3.D.f << endl << endl;
+	cout << "Real log: " << logf(123.231) << endl << endl << endl;
+	if ((v3.D.man == ch3.D.man) && (v3.D.ex == ch3.D.ex) && (v3.D.ex == ch3.D.ex)) cout << "They equal" << endl;
+	else cout << "They not equal" << endl;
+
+
+
+	double dl = 3.2345;
+	Floating_Point<11, 52> v4(dl);
+
+	double f4 = exp(dl);
+	Floating_Point<11, 52> ch4(f4);
+
+	v4 = v4.exp_function(100000);
+
+	cout << " exp :" << endl << endl;
+	cout << "My exp: " << v4.D.f << endl << endl;
+	cout << "Real exp: " << ch4.D.f << endl << endl << endl;
+	if (v4.D.f==ch4.D.f) cout << "They equal" << endl;
+	else cout << "They not equal" << endl;
 }
---
+++ new_double_3/Double_union/Double_union/Floating_Point.h
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <climits>
+#include <float.h>
+#include <exception>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+
+using std::isinf;
+
+using std::invalid_argument;
+
+
+
+union Dbl
+{
+    double f;
+    struct
+    {
+        uint64_t man : 52;
+        uint64_t ex : 11;
+        uint64_t sign : 1;
+    };
+};
+
+
+template<int e, int m>
+class Floating_Point
+{
+private:
+
+public:
+
+    Dbl D;
+
+    unsigned long long int get_mask() noexcept
+    {
+        unsigned long long int one = 1;
+        unsigned long long int mask = (((one << (m + 11)) - 1) << 1) + 1;
+        mask = mask << (52 - m);
+
+        return mask;
+    }
+
+    Floating_Point() noexcept
+    {
+        D.f = 0.0;
+    }
+    Floating_Point(const double& a)  noexcept
+    {
+        D.f = a;
+        CHECK_EXP_AND_MAN();
+    }
+    Floating_Point(const Floating_Point& a) noexcept
+    {
+        D.f = a.D.f;
+    }
+
+    Floating_Point& operator=(const Floating_Point& a) noexcept
+    {
+        D.f = a.D.f;
+        return *this;
+    }
+    Floating_Point operator+(const Floating_Point& a)
+    {
+        Floating_Point res(*this);
+        res.D.f += a.D.f;
+        res.CHECK_EXP_AND_MAN();
+        return res;
+    }
+    Floating_Point operator-(const Floating_Point& a)
+    {
+        Floating_Point res(*this);
+        res.D.f -= a.D.f;
+        res.CHECK_EXP_AND_MAN();
+        return res;
+    }
+    Floating_Point operator*(const Floating_Point& a)
+    {
+        Floating_Point res(*this);
+        res.D.f *= a.D.f;
+        res.CHECK_EXP_AND_MAN();
+        return res;
+    }
+    Floating_Point operator/(const Floating_Point& a)
+    {
+        Floating_Point res(*this);
+        res.D.f /= a.D.f;
+        res.CHECK_EXP_AND_MAN();
+        return res;
+    }
+
+    bool Check_expMAX() noexcept
+    {
+        return ((1024 + ((1 << (e - 1)) - 1)) >= D.ex);
+    }
+    bool Check_expMIN() noexcept
+    {
+        return ((1023 - ((1 << (e - 1)) - 1)) <= D.ex);
+    }
+
+    void special_infinity_and_null_check() noexcept
+    {
+        D.man &= (get_mask() * Check_expMAX());
+        D.ex |= ((1 << 11) - 1) * (unsigned long long int)!(Check_expMAX());
+
+        D.man &= (get_mask() * Check_expMIN());
+        D.ex &= ((1 << 11) - 1) * (unsigned long long int)(Check_expMIN());
+    }
+
+    void CHECK_EXP_AND_MAN() noexcept
+    {
+        special_infinity_and_null_check();
+
+    }
+
+
+
+
+    Floating_Point exp_function(size_t n) // n - количество элементов ряда 
+    {
+        Floating_Point a(*this);
+        a = a * log2(exp(1.0));
+        int ipart = FLOOR(a+0.5);
+        Floating_Point fpart = (a.D.f - ipart)* log(2.0);//x*ln2
+        Floating_Point last = 1.0,next;
+        a = last;
+
+        for (size_t i = 1; i < n; i++)
+        {
+            next = fpart/i;
+            a = MY_FMA(last, next, a);
+            last = last*next;
+        }
+
+        a = a * exp2(ipart);
+
+        return a;
+    }
+
+
+
+
+    void get_double() noexcept
+    {
+        cout << "Double: " << D.f << endl;
+        cout << "sign: " << D.sign << endl;
+        cout << "exp: " << D.ex << endl;
+        cout << "mantissa: " << D.man << endl;
+    }
+
+
+
+
+    Floating_Point Log2(Floating_Point& a)
+    {
+        Floating_Point q(a);
+        double n1 = log2(1.5); // логарифм числа 1.5 по основанию 2
+        double n2 = log(2); // логарифм числа 2 по основанию e
+        q.D.f = q.D.f / (2 << (q.D.ex - 1023 - 1));
+        q.D.f = q.D.f / 1.5;
+        q.D.f = q.D.f - 1.0;                                        //                                       log2(y)= ln(y)/ln(2)         ln(1+x)=x - x^2/2 + x^3/3 - ...             
+        Floating_Point mclrn = q.D.f;
+        Floating_Point blog = q.D.f;
+        Floating_Point alog;
+        for (size_t i = 2; i < 10; i++)
+        {
+            alog = q.D.f / double(i) * (-1.0) * (i-1);
+            mclrn = MY_FMA(blog, alog, mclrn);
+            blog = blog * alog;
+        }
+
+
+        a =  (mclrn) / n2  + n1+ (a.D.ex - 1023); // ( x - x^2/2 + x^3/3 - ... + x^7/7)/ln(2) + log2(1.5) + exp - (погрешность) .   x = ((исходное число)/(2^exp))/1.5 - 1 .
+
+        return a;
+    }
+
+    Floating_Point Log(Floating_Point& a, Floating_Point& q)
+    {
+        a = Log2(a) / Log2(q);
+        return a;     
+    }
+
+    Floating_Point Log10(Floating_Point& a)
+    {
+        Floating_Point ten = 10.0;
+        a = Log2(a) / Log2(ten);
+        return a;
+    }
+
+    Floating_Point LogE(Floating_Point& a)
+    {
+        Floating_Point expanenta = 2.7182818284;
+        a = Log2(a) / Log2(expanenta);
+        return a;
+    }
+
+};
+
+template<int e,int m>
+int FLOOR(Floating_Point<e,m> a)
+{
+    return floor(a.D.f+0.5);
+}
+template<int e, int m>
+Floating_Point<e, m> MY_FMA(Floating_Point<e, m>& a, Floating_Point<e, m>& b, Floating_Point<e, m>& c)
+{
+    return a * b + c;
+}
